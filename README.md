8aadfc695d608df48408e5c0574c9e99cba153a5
fabiconcept <favourajokubi@gmail.com>
2024-12-29 08:18:58 +0100

    `Add new sections to README.md and update TextInput components in app/tabs/index.tsx`

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [https://www.youtube.com/watch?v=ny7ZWVt3ymo&list=PLC3y8-rFHvwhiQJD1di4eRVN30WWCXkg1&index=3](Visit Link)
 
 - Part 57 : Inputs and Forms ✅
+- Part 58 : TextInput Props ✅
+- Part 59 : Multiline TextInput ✅
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,12 +9,23 @@ export default function index() {
         <SafeAreaView style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
             <Text>My Name is {name}</Text>
-            
+
             <TextInput
                 style={styles.input}
                 placeholder="Enter your name"
                 value={name}
                 onChangeText={(text) => setName(text)}
+                secureTextEntry
+                keyboardType="default"
+                // autoCorrect={false}
+                // autoCapitalize="none"
+            />
+
+            <TextInput
+                style={[styles.multiline, styles.input]}
+                placeholder="Message"
+                multiline
+                textAlignVertical="top"
             />
         </SafeAreaView>
     )
@@ -25,6 +36,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: "plum",
         padding: 10,
+        gap: 10
     },
     input: {
         height: 40,
@@ -32,5 +44,8 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         padding: 10,
         borderRadius: 10,
+    },
+    multiline: {
+        minHeight: 100,
     }
 })
