b9c57d4e43c89445e8e1b37d7405ae284e746d8e
fabiconcept <favourajokubi@gmail.com>
2024-12-29 02:14:34 +0100

    `Add TextInput and state management to index.tsx`

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,21 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useState } from "react";
 
 export default function index() {
+    const [name, setName] = useState("");
+
     return (
         <SafeAreaView style={styles.container}>
             <Text>Open up App.js to start working on your app!</Text>
+            <Text>My Name is {name}</Text>
+            
+            <TextInput
+                style={styles.input}
+                placeholder="Enter your name"
+                value={name}
+                onChangeText={(text) => setName(text)}
+            />
         </SafeAreaView>
     )
 }
@@ -15,4 +26,11 @@ const styles = StyleSheet.create({
         backgroundColor: "plum",
         padding: 10,
     },
+    input: {
+        height: 40,
+        borderColor: "gray",
+        borderWidth: 2,
+        padding: 10,
+        borderRadius: 10,
+    }
 })
